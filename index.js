fbd2dd485d3c53b4b9cbd0fc2bb1d1d1a1557c33
Musharraf8543 <musharrafhussain4397@gmail.com>
2022-12-01 16:02:38 +0530

    update

--- index.js
+++ index.js
@@ -1,81 +1,79 @@
-const rightContainer= document.getElementsByClassName("right")[0];
-const bombs = [];
-let score = 0;
-let gameOver=false;
-const visitedDiv=[];
+const rightContainer = document.getElementsByClassName("right")[0];
+const bombsArray = [];
+let point = 0;
+let gameOver = false;
+const visitedCell = [];
 
-function createGrid(){
-    for(let i=0;i<9;i++){
-        const row=document.createElement("div");
-        rightContainer.appendChild(row);
-        row.classList.add("cellRow");
-        for(let j=0;j<9;j++){
-            const column=document.createElement("div");
-            row.appendChild(column);
-            column.classList.add("cellColumn");
+function createGrid() {
+  placeBombs();
+  for (let i = 0; i < 9; i++) {
+    const row = document.createElement("div");
+    rightContainer.appendChild(row);
+    row.classList.add("cellRow");
+    for (let j = 0; j < 9; j++) {
+      const column = document.createElement("div");
+      row.appendChild(column);
+      column.classList.add("cellColumn");
 
-            const currentIndex = i*9+j;
-            if(bombs.includes(currentIndex))
-            {
-                column.className = "bomb";
+      const currentIndex = i * 9 + j;
+      if (bombsArray.includes(currentIndex)) {
+        column.classList.add("bomb");
+      }
+      column.addEventListener("click", () => {
+        if (!gameOver) {
+          if (bombsArray.includes(currentIndex)) {
+            column.style.background = "red";
+            gameOver = true;
+            showAllBombs();
+          } else {
+            column.style.background = "green";
+            if (!visitedCell.includes(currentIndex)) {
+              incrementScore();
+              visitedCell.push(currentIndex);
             }
-            column.addEventListener("click",()=>{
-                if(!gameOver){
-                    if(bombs.includes(currentIndex))
-                    {
-                        column.style.background = "red";
-                        gameOver=true;
-                        showAllBombs();
-                    }
-                    else
-                    {
-                        column.style.background = "green";
-                        if(!visitedDiv.includes(currentIndex)){
-                            incrementScore();
-                            visitedDiv.push(currentIndex);
-                        }
-                    }
-                }
-            })
+          }
         }
+      });
     }
+  }
 }
 
-function showAllBombs(){
-    const allBombs=document.getElementsByClassName("bomb");
-    for(let bomb of allBombs){
-        bomb.style.background = "red";
+function showAllBombs() {
+  const allBombs = document.getElementsByClassName("bomb");
+  for (let bomb of allBombs) {
+    bomb.style.background = "red";
+  }
+}
+
+// const resetButton = document.getElementById("reset");
+// resetButton.addEventListener("click", () => {
+//   location.reload();
+// });
+
+function reload() {
+  location.reload()
+}
+
+function incrementScore() {
+  point++;
+  const scoreElement = document.getElementById("score");
+  scoreElement.innerText = point;
+}
+
+function generateRandomNumber() {
+  let randomNumber = Math.random() * 81;
+  randomNumber = parseInt(randomNumber);
+  return randomNumber;
+}
+function placeBombs() {
+  while (bombsArray.length != 10) {
+    const randomNumber = generateRandomNumber();
+    if (!bombsArray.includes(randomNumber)) {
+      bombsArray.push(randomNumber);
     }
+  }
 }
-
-const resetButton=document.getElementById("reset");
-resetButton.addEventListener("click",()=>{
-    window.location.reload();
-})
-
-function incrementScore(){
-    score++;
-    const scoreElement=document.getElementById("score");
-    scoreElement.innerText = score;
-}
-function generateRandomNumber(){
-    let randomNumber=Math.random();
-    randomNumber=randomNumber.toFixed(2);
-    randomNumber*=100;
-    randomNumber=randomNumber % 81;
-    randomNumber=parseInt(randomNumber);
-    return randomNumber;
-}
-function placeBombs(){
-    while(bombs.length!=10){
-        const randomNumber=generateRandomNumber();
-        if(!bombs.includes(randomNumber)){
-            bombs.push(randomNumber);
-        }
-    }
-}
-
-placeBombs();
 createGrid();
+
 //generateRandomNumber();
-console.log(bombs);
+console.log(bombsArray);
